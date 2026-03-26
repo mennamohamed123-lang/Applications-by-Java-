c6a9b5928a6cf072bf0b2cd4b27ba5eabccd3511
Shrouk298 <shroukashraf8282@gmail.com>
2026-03-26 02:50:19 +0200

    Add files via upload

---
+++ QuizGame/student.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.javaproj;
+
+/**
+ *
+ * @author Sho
+ */
+public class student {
+    private String name;
+    private String category;
+    private int score;
+    public void set_name(String n){
+        
+                
+        name = n;
+    }
+    public String get_name(){
+       return name;
+    }
+    public void set_category(String c){
+        category = c;
+    }
+    public String get_category(){
+         return category;
+    }
+    public void set_score(int s){
+        score = s;
+    }
+    public int get_score(){
+       return score;
+    }
+
+    
+}
